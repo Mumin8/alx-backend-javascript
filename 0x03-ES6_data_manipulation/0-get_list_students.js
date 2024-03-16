c85d6cf64ade4b6e6bf82bf5bd54af1cbb8d214d
Mumin8 <alhassanmumin8@gmail.com>
2024-03-16 23:03:30 +0000

    readme

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,11 @@
-export default function getListStudents() {
+// 0-get_list_students.js
+const  getListStudents = () => {
     return [
       { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
       { id: 2, firstName: 'James', location: 'Columbia' },
-      { id: 5, firstName: 'Serena', location: 'San Francisco' },
+      { id: 5, firstName: 'Serena', location: 'San Francisco' }
     ];
   }
+  
+  export default getListStudents;
+  
