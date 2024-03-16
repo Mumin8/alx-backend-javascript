f52ccb86a838d5c5ede1d3465e1e8bf2382e5056
Mumin8 <alhassanmumin8@gmail.com>
2024-03-16 23:11:18 +0000

    es6

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,11 +1,8 @@
-// 1-get_list_student_ids.js
-function getListStudentIds(array) {
+const getListStudentIds = (array) => {
     if (!Array.isArray(array)) {
       return [];
     }
-  
     return array.map(item => item.id);
   }
-  
   export default getListStudentIds;
   
